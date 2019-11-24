c8c9a6827382380df4cdeddb8c80dcd058407a2f
eranboudjnah <shadowcra@gmail.com>
2019-11-24 19:44:46 +0000

    Added documentation. Minor import tidy up

--- solidrecyclerview/src/main/java/com/mitteloupe/solid/recyclerview/ViewBinder.kt
+++ solidrecyclerview/src/main/java/com/mitteloupe/solid/recyclerview/ViewBinder.kt
@@ -1,8 +1,32 @@
 package com.mitteloupe.solid.recyclerview
 
-import androidx.recyclerview.widget.RecyclerView
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView.Adapter
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
 
-interface ViewBinder<VIEW_HOLDER : RecyclerView.ViewHolder, DATA> {
+/**
+ * Classes implementing this interface bind the [View]s held by [ViewHolder]s to provided data,
+ * optionally taking a collection of change payloads.
+ *
+ * @See ViewHolder
+ * @See SolidAdapter.onBindViewHolder
+ * @See Adapter.onBindViewHolder
+ */
+interface ViewBinder<VIEW_HOLDER : ViewHolder, DATA> {
+    /**
+     * Binds [data] to the [View]s held by [viewHolder].
+     *
+     * @See SolidAdapter.onBindViewHolder
+     * @See Adapter.onBindViewHolder
+     */
     fun bindView(viewHolder: VIEW_HOLDER, data: DATA)
+
+    /**
+     * Binds [data] to the [View]s held by [viewHolder], taking into account the changes specified
+     * in [payloads].
+     *
+     * @See SolidAdapter.onBindViewHolder
+     * @See Adapter.onBindViewHolder
+     */
     fun bindView(viewHolder: VIEW_HOLDER, data: DATA, payloads: Set<Any>)
 }

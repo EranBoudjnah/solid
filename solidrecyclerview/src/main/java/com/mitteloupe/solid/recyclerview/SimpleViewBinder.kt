cbaf66fe17ffeb9807583000463bed0a5bbd9fd5
eranboudjnah <shadowcra@gmail.com>
2019-11-24 19:47:09 +0000

    Added documentation

--- solidrecyclerview/src/main/java/com/mitteloupe/solid/recyclerview/SimpleViewBinder.kt
+++ solidrecyclerview/src/main/java/com/mitteloupe/solid/recyclerview/SimpleViewBinder.kt
@@ -2,7 +2,18 @@ package com.mitteloupe.solid.recyclerview
 
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 
+/**
+ * A simplified [ViewBinder], ignoring any payloads when binding data.
+ *
+ * @see ViewBinder
+ */
 abstract class SimpleViewBinder<VIEW_HOLDER : ViewHolder, DATA> : ViewBinder<VIEW_HOLDER, DATA> {
+    /**
+     * Delegates any calls providing [payloads] to the [ViewBinder.bindView] implementation that does
+     * not take [payloads] into account when binding.
+     *
+     * @see ViewBinder.bindView
+     */
     override fun bindView(viewHolder: VIEW_HOLDER, data: DATA, payloads: Set<Any>) =
         bindView(viewHolder, data)
 }

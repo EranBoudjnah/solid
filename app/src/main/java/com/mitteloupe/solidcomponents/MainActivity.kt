dc593b7a6a761aa95a874773e29d3b1b4f8392a7
eranboudjnah <shadowcra@gmail.com>
2019-11-29 11:18:10 +0000

    Addressed ktlint issues. Updated SolidActivity version to 1.0.3

--- app/src/main/java/com/mitteloupe/solidcomponents/MainActivity.kt
+++ app/src/main/java/com/mitteloupe/solidcomponents/MainActivity.kt
@@ -3,14 +3,14 @@ package com.mitteloupe.solidcomponents
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.mitteloupe.solid.recyclerview.SolidAdapter
 import com.mitteloupe.solid.activity.SolidActivity
 import com.mitteloupe.solid.activity.handler.common.LayoutLifecycleHandler
+import com.mitteloupe.solid.recyclerview.SolidAdapter
 import com.mitteloupe.solidcomponents.adapter.MoodViewHolder
 import com.mitteloupe.solidcomponents.hander.KoinActivityScopeHandler
 import com.mitteloupe.solidcomponents.model.MoodUiModel
-import org.koin.android.scope.currentScope
 import kotlinx.android.synthetic.main.activity_main.recycler_view as recyclerView
+import org.koin.android.scope.currentScope
 
 class MainActivity : SolidActivity() {
     private val solidAdapter: SolidAdapter<MoodViewHolder, MoodUiModel> by currentScope.inject()
@@ -55,4 +55,3 @@ class MainActivity : SolidActivity() {
         )
     }
 }
-

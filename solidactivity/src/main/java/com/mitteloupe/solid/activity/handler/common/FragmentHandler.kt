29c054ef0487bc7635e9cf4f647088a7519800b1
eranboudjnah <shadowcra@gmail.com>
2019-12-03 11:42:28 +0000

    Added support for FragmentFactory

--- solidactivity/src/main/java/com/mitteloupe/solid/activity/handler/common/FragmentHandler.kt
+++ solidactivity/src/main/java/com/mitteloupe/solid/activity/handler/common/FragmentHandler.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.annotation.IdRes
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentFactory
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.commit
 import com.mitteloupe.solid.activity.handler.LifecycleHandler
@@ -11,8 +12,15 @@ import com.mitteloupe.solid.activity.handler.LifecycleHandler
 class FragmentHandler(
     private val supportFragmentManager: FragmentManager,
     @IdRes private val mainFragmentContainer: Int,
-    private val fragmentClass: Class<out Fragment>
+    private val fragmentClass: Class<out Fragment>,
+    private val fragmentFactory: Lazy<FragmentFactory>? = null
 ) : LifecycleHandler {
+    override fun onPreCreate(savedInstanceState: Bundle?) {
+        fragmentFactory?.let { fragmentFactory ->
+            supportFragmentManager.fragmentFactory = fragmentFactory.value
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         supportFragmentManager.commit {
             replace(mainFragmentContainer, getFragmentInstance(fragmentClass))

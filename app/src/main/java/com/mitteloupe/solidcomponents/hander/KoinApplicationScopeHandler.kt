ec7325b5d3b0345abe418a75a424d529cfaf6bdd
Eran Boudjnah <eranbou@gmail.com>
2021-04-11 16:42:48 +0100

    Fixed Koin related issue.

--- app/src/main/java/com/mitteloupe/solidcomponents/hander/KoinApplicationScopeHandler.kt
+++ app/src/main/java/com/mitteloupe/solidcomponents/hander/KoinApplicationScopeHandler.kt
@@ -6,13 +6,14 @@ import com.mitteloupe.solidcomponents.di.appModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
 
 class KoinApplicationScopeHandler(
     private val application: Application
 ) : LifecycleHandler {
     override fun onCreate() {
         startKoin {
-            androidLogger()
+            androidLogger(Level.ERROR)
             androidContext(application)
             modules(appModule)
         }
